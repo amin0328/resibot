b7e6f6482bad80add29c3bf3283acb8c963fc6e9
amin0328 <amin060328@gmail.com>
2026-03-20 01:22:03 +0800

    config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+TOKEN = "0" # omitted
+RC_OPTIONS = ["CAPT", "Tembusu", "Acacia", "NUSC", "RVRC", "RC4"]
+
+HOUSE_OPTIONS_BY_RC = {
+    "CAPT": ["Tulpar", "Phoneix", "Garuda", "Dragon", "Roc"],
+    "Tembusu": ["Shan", "Ora", "Gaja", "Tancho", "Ponya"],
+    "Acacia": ["Aeon", "Nous", "Zenith"],
+    "NUSC": ["Kairos", "Levios", "Idalia", "Osceanna", "Corvex", "Perseus"],
+    "RVRC": ["Aonyx", "Chelonia", "Manis", "Orcaella", "Panthera", "Rusa", "Strix"],
+    "RC4": ["Aquila", "Noctua", "Ursa", "Leo", "Draco"]
+}
+
+INTEREST_OPTIONS = [
+    "sports",
+    "performing arts",
+    "arts",
+    "food & cooking",
+    "wellness",
+    "social",
+    "personal development"
+]
+
+USERS_FILE = "data/users.json"
+EVENTS_FILE = "data/events.json"
+RC_ROLE_FOLDER = "data/rc_roles"
